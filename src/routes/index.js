102a0e60e432b10b0cdb6410cd578318db64e73b
quy <quylvph19871@gmail.com>
2024-06-11 10:11:30 +0700

    source

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import HomePage from "../components/HomePage/HomePage";
+import MoviePage from "../components/MoviePage/MoviePage";
+
+
+
+export const routes = [
+    {
+        path: '/',
+        page: HomePage
+    },
+    {
+        path: '/movie',
+        page: MoviePage
+    }
+]
